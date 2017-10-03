7905505c9483dad59941adaab5cac087ccf89013
kiryanenko <kiryanekoav@mail.ru>
2017-10-03 13:04:40 +0300

    Тесты на валидацию

--- src/test/java/application/services/UserServiceTest.java
+++ src/test/java/application/services/UserServiceTest.java
@@ -40,8 +40,22 @@ public class UserServiceTest {
     @Test
     public void testCreate() {
         create(credentials);
-        final ErrorResponseList errors = userService.create(credentials);
+
+        ErrorResponseList errors = userService.create(credentials);
         assertFalse("Не выдало ошибки об существовании такого же пользователя", errors.isEmpty());
+
+        // Тесты на валидацию
+        User notValidUser = new User(null, "log *-in" + ID_GENERATOR.getAndIncrement(), "password", "user@mail.ru");
+        errors = userService.create(notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность логина", errors.isEmpty());
+
+        notValidUser = new User(null, "login" + ID_GENERATOR.getAndIncrement(), "pass word", "user@mail.ru");
+        errors = userService.create(notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность пароля", errors.isEmpty());
+
+        notValidUser = new User(null, "login" + ID_GENERATOR.getAndIncrement(), "password", "usermail.ru");
+        errors = userService.create(notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность почты", errors.isEmpty());
     }
 
 
@@ -61,6 +75,11 @@ public class UserServiceTest {
         final User updatedUser = update(credentials.getId(), newCredentials);
         assertTrue("Пароль не изменился при обновлении пароля и почты", userService.checkpw(newCredentials.getPassword(), updatedUser.getPassword()));
         assertEquals("Почта не изменилась при обновлении пароля и почты", updatedUser.getEmail(), newCredentials.getEmail());
+
+        // Тесты на валидацию
+        User notValidUser = new User(null, null, "pass word", "Usermail.ru");
+        ErrorResponseList errors = userService.update(credentials.getId(), notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность", errors.isEmpty());
     }
 
 
@@ -70,6 +89,11 @@ public class UserServiceTest {
         final User newCredentials = new User(null, null, "newPassword", null);
         final User updatedUser = update(credentials.getId(), newCredentials);
         assertTrue("Пароль не изменился при обновлении пароля", userService.checkpw(newCredentials.getPassword(), updatedUser.getPassword()));
+
+        // Тесты на валидацию
+        User notValidUser = new User(null, null, "pass word", null);
+        ErrorResponseList errors = userService.update(credentials.getId(), notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность", errors.isEmpty());
     }
 
 
@@ -79,5 +103,10 @@ public class UserServiceTest {
         final User newCredentials = new User(null, null, null, "newuser@mail.ru");
         final User updatedUser = update(credentials.getId(), newCredentials);
         assertEquals("Почта не изменилась при обновлении почты", updatedUser.getEmail(), newCredentials.getEmail());
+
+        // Тесты на валидацию
+        User notValidUser = new User(null, null, "password", "newuser@mailru");
+        ErrorResponseList errors = userService.update(credentials.getId(), notValidUser);
+        assertFalse("Не выдало ошибки при проверки на валидность", errors.isEmpty());
     }
 }
