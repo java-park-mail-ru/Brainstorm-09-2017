8597ac42c71d2f2f6c2224d7aca35ec7107c1fbc
kiryanenko <KiryanenkoAV@mail.ru>
2017-10-16 23:02:54 +0300

    Добавил testSignup()

--- src/test/java/application/controllers/UsersControllerTest.java
+++ src/test/java/application/controllers/UsersControllerTest.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -39,13 +42,15 @@ public class UsersControllerTest {
                 .andExpect(status().isUnauthorized());
     }
 
-// FIXME
-//    @Test
-//    public void testSignup() throws IOException {
-//        when(credentials.ensureUserExists(anyString())).thenReturn(new User("foo"));
-//        final Response<SuccessResponse> response = app.signup(credentials).execute();
-//        assertEquals(HttpStatus.OK.value(), response.code());
-//    }
+
+    @Test
+    public void testSignup() throws Exception {
+        mockMvc
+                .perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"login\":\"login\", \"password\":\"password\", \"email\":\"user@mail.ru\"}"))
+                .andExpect(status().isOk());
+    }
 //
 //
 //    @Test
